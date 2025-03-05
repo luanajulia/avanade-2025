98dd6d05dbe9b6ccb443965e479b87d81ebd3f21
Luana Julia <luana.ju.gomes@gmail.com>
2025-03-05 20:41:27 -0300

    05032025

---
+++ src/test/java/dio/avanade/avanade_2025/Avanade2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.avanade.avanade_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Avanade2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
